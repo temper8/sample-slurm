9a7e84e63aacb44f646bf6cb17dbe49b946c815a
Alex <temper8@gmail.com>
2025-10-12 11:50:13 +0300

    Update s1.py

--- s1.py
+++ s1.py
@@ -1,10 +1,10 @@
 from simple_slurm import Slurm
 
 slurm = Slurm(
-    array=range(1, ),
+    array=range(1, 11),
     cpus_per_task=2,
     job_name='demo_name',
-    dependency=dict(after=65541, afterok=34987),
+    #dependency=dict(after=65541, afterok=34987),
     output=f'{Slurm.JOB_ARRAY_MASTER_ID}_{Slurm.JOB_ARRAY_ID}.out',
 )
 slurm.sbatch(f'echo task_No_{Slurm.SLURM_ARRAY_TASK_ID}')
