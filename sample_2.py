f6ad00f46cd00d41dba3f86ad5a8a7980d93f9d9
Alex <temper8@gmail.com>
2025-10-13 14:09:46 +0300

    Update sample_2.py

--- sample_2.py
+++ sample_2.py
@@ -20,4 +20,4 @@ slurm.add_cmd("work_dir=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTask
 slurm.add_cmd('echo "Environment setup complete"')
 print(slurm)
 # Print to a file a message that includes the current $SLURM_ARRAY_TASK_ID and work_dir
-slurm.sbatch('demo.sh ${SLURM_ARRAY_TASK_ID} ${work_dir}.')
+slurm.sbatch('./demo.sh ${SLURM_ARRAY_TASK_ID} ${work_dir}.')
