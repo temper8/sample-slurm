b4353e5e89300c61173e58f625d1a681dfbfe43a
Alex <temper8@gmail.com>
2025-10-14 13:36:16 +0300

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -3,9 +3,9 @@ tasks_array=task_array.txt
 task_dir=$(awk -v ArrayTaskID=$1 '$1==ArrayTaskID {print $2}' $tasks_array)
 echo "This is array task $1, the work_dir is $task_dir."
 START=$(date +%s)
-echo "Start : $(START)"
+echo "Start : $START"
 sleep 30
 END=$(date +%s)
 DIFF=$(( $END - $START ))
-echo "End : $(END)"
+echo "End : $END"
 echo "It took $DIFF seconds"
