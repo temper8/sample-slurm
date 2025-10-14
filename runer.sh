9646aa41be6634f6ef3246234a7f64736f2ddffe
Alex <temper8@gmail.com>
2025-10-14 15:52:14 +0300

    Update runer.sh

--- runer.sh
+++ runer.sh
@@ -1,7 +1,7 @@
 hostname
 echo "Task $1, job_dir $2."
-tasks_array=$2 + /task_array.txt
-task_dir=$2 + '/' +  $(awk -v ArrayTaskID=$1 '$1==ArrayTaskID {print $2}' $tasks_array)
+tasks_array=$2 'task_array.txt'
+task_dir=$2 '/' $(awk -v ArrayTaskID=$1 '$1==ArrayTaskID {print $2}' $tasks_array)
 echo "This is array task $1, the work_dir is $task_dir."
 START=$(date +%s)
 echo "Start : $START"
